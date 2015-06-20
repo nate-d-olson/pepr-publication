23e23f147fe183ec3b56f0fdfcbd2772d2956287
Nate Olson <nathandavidolson@gmail.com>
2015-06-19 21:09:14 -0400

    results supporting files

---
+++ rm_metadata.R
@@ -0,0 +1,14 @@
+# When using this template change for RM specific information
+rm_number <- "RM8376"
+rm_strain <- "Staphlyococcus aureus NGS100"
+rm_genus <- "Staphylococcus"
+rm_concentration <- 50
+rm_volume <- 60
+rm_mass <- round(rm_concentration*rm_volume/1000,0)
+rm_vial_number <- 1500
+strain_source <- "Children's National Hostipal."
+rm_genome_size <- 2.8*10^6
+
+# Database location
+db_path <- "/Volumes/NDO_PROJECT/current_projects/micro_rm//micro_rm/pepr-data/MG002/MG002.sqlite"
+# db_path <- "/media/nolson/second2/current_projects/micro_rm/pepr-data/MG002/CFSAN030013_miseq.sqlite"
