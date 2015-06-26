8b11278562918c39df7279f26a0de806f947ecd2
Nate Olson <nathandavidolson@gmail.com>
2015-06-26 12:07:03 -0400

    code for calculating values for text

---
+++ calc_results_values.R
@@ -0,0 +1,5 @@
+pos_pur_gt99_both <- 0
+pos_pur_gt99_one <- 0
+pos_pur_gt95_one <- 0
+min_max_pur_position  <- paste0(as.character(round(0, 2)*100),"%") # minimum purity for at least one platform (min, max), return %
+max_contam <- paste0(as.character(round(1, 2)*100),"%") # maximum contamination per dataset
